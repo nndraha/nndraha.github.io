dec85b1d73f69d6194a370f8c08ac6128ce936b4
nndraha <168002774+nndraha@users.noreply.github.com>
2026-05-11 09:07:37 +0000

    Deploying to gh-pages from @ nndraha/nndraha.github.io@cc1e9060a5c8a91848c4083f6fb324987ffef5a2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-research-team",
           title: "Research Team",
-          description: "members of the lab or group",
+          description: "Members of the lab or group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
