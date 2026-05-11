57cbf2f8e81b62f9d88ca7171661757107239f45
nndraha <168002774+nndraha@users.noreply.github.com>
2026-05-11 08:48:22 +0000

    Deploying to gh-pages from @ nndraha/nndraha.github.io@c2ab72d56d8ad5e66e8a1f5c79999e2374950dbd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-services",
+          title: "Services",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-research-team",
+          title: "Research Team",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
